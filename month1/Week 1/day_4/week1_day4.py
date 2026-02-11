12edb7f4fb3d4b38f8155b9bc2d3a1c7269b94ae
Arellano-Kelvin <arellano.kelvin.91@gmail.com>
2026-02-11 05:51:58 -0700

    updated functons

--- month1/Week 1/day_4/week1_day4.py
+++ month1/Week 1/day_4/week1_day4.py
@@ -1,13 +1,17 @@
 def greet(name):
     return f'Hello {name}'
 def if_even(number):
-    if number % 2 == 0:
-        return True
-    else:
-        return False
+    return number % 2 == 0
 def calculate_area(length, width):
-    return length * width
+    if length > 0 & width > 0:
+        return length * width
+    else:
+        print('Please input valid number')
 def find_max(list_of_numbers):
     return max(list_of_numbers)
 def validate_age(age):
-    return age<=120
+    if len(age) >=0:
+        try:
+            return age<=120
+        except TypeError:
+            age = []
