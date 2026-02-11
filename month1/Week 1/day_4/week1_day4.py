00f0e3d23fd607a60531482bdef5f1575e93990b
Arellano-Kelvin <arellano.kelvin.91@gmail.com>
2026-02-11 04:55:18 -0700

    added beginning functions

---
+++ month1/Week 1/day_4/week1_day4.py
@@ -0,0 +1,13 @@
+def greet(name):
+    return f'Hello {name}'
+def if_even(number):
+    if number % 2 == 0:
+        return True
+    else:
+        return False
+def calculate_area(length, width):
+    return length * width
+def find_max(list_of_numbers):
+    return max(list_of_numbers)
+def validate_age(age):
+    return age<=120
