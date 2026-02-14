dc84b92a565b7d7756923cbe086182f76eead7cc
Arellano-Kelvin <arellano.kelvin.91@gmail.com>
2026-02-14 15:28:01 -0700

    exercism func

--- month1/practice/exercism/python/guidos-gorgeous-lasagna/lasagna.py
+++ month1/practice/exercism/python/guidos-gorgeous-lasagna/lasagna.py
@@ -10,9 +10,13 @@ of a module and its functions and/or classes.
 
 #TODO: define your EXPECTED_BAKE_TIME (required) and PREPARATION_TIME (optional) constants below.
 
+EXPECTED_BAKE_TIME=40
+PREPARARTION_TIME=20
 
 #TODO: Remove 'pass' and complete the 'bake_time_remaining()' function below.
-def bake_time_remaining():
+def bake_time_remaining(elapsed_bake_time):
+    return int(EXPECTED_BAKE_TIME-elapsed_bake_time)
+
     """Calculate the bake time remaining.
 
     :param elapsed_bake_time: int - baking time already elapsed.
@@ -23,19 +27,17 @@ def bake_time_remaining():
     based on the `EXPECTED_BAKE_TIME`.
     """
 
-    pass
-
 
 #TODO: Define the 'preparation_time_in_minutes()' function below.
 # To avoid the use of magic numbers (see: https://en.wikipedia.org/wiki/Magic_number_(programming)), you should define a PREPARATION_TIME constant.
 # You can do that on the line below the 'EXPECTED_BAKE_TIME' constant.
 # This will make it easier to do calculations, and make changes to your code.
-
-
+def preparation_time_in_minutes(number_of_layers):
+    return int(number_of_layers*2)
 
 #TODO: define the 'elapsed_time_in_minutes()' function below.
-
-
+def elapsed_time_in_minutes(number_of_layers,elapsed_bake_time):
+    return int((number_of_layers*2)+elapsed_bake_time)
 
 # TODO: Remember to go back and add docstrings to all your functions
 #  (you can copy and then alter the one from bake_time_remaining.)
