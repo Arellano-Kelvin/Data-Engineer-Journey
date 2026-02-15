453d9b3363a295c5e26a3a97f437e36577fe9ada
Arellano-Kelvin <arellano.kelvin.91@gmail.com>
2026-02-14 17:35:24 -0700

    added error message and finished second exercises

--- month1/Week 1/day_4/week1_day4.py
+++ month1/Week 1/day_4/week1_day4.py
@@ -3,15 +3,18 @@ def greet(name):
 def if_even(number):
     return number % 2 == 0
 def calculate_area(length, width):
-    if length > 0 & width > 0:
+    if length > 0 and width > 0:
         return length * width
     else:
         print('Please input valid number')
 def find_max(list_of_numbers):
+    if len(list_of_numbers) == 0:
+        return None
     return max(list_of_numbers)
+
 def validate_age(age):
-    if len(age) >=0:
         try:
-            return age<=120
+            if 120 >= age >= 0:
+                return age<=120
         except TypeError:
-            age = []
+            age = 0
